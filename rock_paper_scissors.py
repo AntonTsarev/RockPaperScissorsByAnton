19f32837c9edb0b0ab86270cd7e1a7596c5bfd78
AntonTsarev <anton.tsarev112@gmail.com>
2023-05-21 14:09:27 +0300

    Added a couple more if statements which allow the game to finish at 3-0 for either the player or computer. Some more minor modifications to the colouring and spacing between responses

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -9,22 +9,34 @@ player_score = 0
 computer_score = 0
 
 while rounds <= 5:
-    if rounds > 3:
+    if rounds == 4 and computer_score != player_score:
         if computer_score > player_score:
-            print(Fore.RED + "SORRY! You lost!")
-            print("Do you want to play another round?... Type YES to continue")
+            print(Fore.RED + "SORRY! You lost the game!")
+            print("Do you want to play another round?... Type YES to continue or ANY other key to EXIT!")
             print()
         elif player_score > computer_score:
             print(Fore.GREEN + "You WON the GAME!")
-            print("Do you want to play another round?... Type YES to continue")
+            print(Fore.CYAN + "Do you want to play another round?... Type 'yes' to continue or ANY other key to EXIT!")
             print()
-
+    elif rounds == 3 and computer_score == 0 or rounds == 3 and player_score == 0:
+        if computer_score > player_score:
+            print(Fore.RED + "SORRY! You lost the game!")
+            print("Do you want to play another round?... Type YES to continue or ANY other key to EXIT!")
+            print()
+        elif player_score > computer_score:
+            print(Fore.GREEN + "You WON the GAME!")
+            print(Fore.CYAN + "Do you want to play another round?... Type 'yes' to continue or ANY other key to EXIT!")
+            print()
         decision_to_continue_the_game = input()
         if decision_to_continue_the_game.upper() == "YES":
+            player_score = 0
+            computer_score = 0
             rounds = 0
+            print(Fore.CYAN + "GREAT! Let's go for another round!")
+            print()
             continue
         else:
-            print("Thank you! Have a nice day!")
+            print(Fore.BLUE + "Thank you! Have a nice day! :)")
             exit()
 
     player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
@@ -36,7 +48,9 @@ while rounds <= 5:
     elif player_move == "s":
         player_move = scissors
     else:
-        raise SystemExit("Invalid Input. Try again...")
+        print(Fore.RED + "Invalid Choice! Please input 'r' for rock, 'p' for paper or 's' for scissors")
+        print()
+        continue
 
     computer_random_number = random.randint(1, 3)
     computer_move = ""
@@ -56,10 +70,14 @@ while rounds <= 5:
         print(Fore.GREEN + "You win!")
         player_score += 1
         rounds += 1
+        continue
     elif computer_move == player_move:
         print(Fore.YELLOW + "Draw!")
+        print()
         continue
     else:
         print(Fore.RED + "You lose!")
+        print()
         computer_score += 1
         rounds += 1
+        continue
